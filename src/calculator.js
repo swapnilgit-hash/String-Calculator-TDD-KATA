34f91e7e50e6503190bad5b340d9b06a56d59797
swapnil <swapnilb@safexpay.com>
2025-10-06 16:04:41 +0530

    method to handle new lines between numbers

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,8 @@
 function add(numbers) {
   if (numbers === "") return 0; // case: empty string
-  if (!numbers.includes(",")) return parseInt(numbers, 10); // case: single number
 
-  return numbers.split(",").reduce((sum, n) => sum + parseInt(n, 10), 0);
+  const parts = numbers.split(/,|\n/); // split by comma or newline
+  return parts.reduce((sum, n) => sum + parseInt(n, 10), 0);
 }
 
 module.exports = add;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -16,3 +16,8 @@ test("handle any amount of number", () => {
   expect(add("1,2,3,4,5")).toBe(15);
   expect(add("10,20,30")).toBe(60);
 });
+
+test("handle new lines between numbers", () => {
+  expect(add("1\n2,3")).toBe(6);
+  expect(add("10\n20\n30")).toBe(60);
+});
